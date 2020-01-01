6f31547e7fa1ac0d3a3c260dc8265655de9f41fd
Victor Johnson <johnsonsirv@gmail.com>
2020-01-02 00:56:09 +0100

    basic rendering ti html

--- src/index.js
+++ src/index.js
@@ -9,8 +9,17 @@ const weatherAPIUrl = (location) => {
 const weatherIcon = icon => `http://openweathermap.org/img/wn/${icon}@2x.png`;
 
 const updateWeatherUI = (data) => {
-  document.getElementById('weather-data').innerHTML = `${JSON.stringify(data)}`;
   document.getElementById('weather-icon').src = weatherIcon(data.weather[0].icon);
+  document.getElementById('weather-location').innerHTML = `Location: ${data.name},${data.sys.country}`;
+  document.getElementById('weather-temp').innerHTML = `Temperature: ${data.main.temp} &#8451;`;
+  document.getElementById('weather-wind').innerHTML = `Wind Speed: ${data.wind.speed} m/s`;
+  document.getElementById('weather-humidity').innerHTML = `Humidity: ${data.main.humidity} %`;
+  document.getElementById('sunrise').textContent = `Sunrise:
+    ${new Date((data.sys.sunrise + data.timezone) * 1000)
+    .toLocaleTimeString().slice(-10, -6)} AM`;
+  document.getElementById('sunset').textContent = `Sunset:
+    ${new Date((data.sys.sunset + data.timezone) * 1000)
+    .toLocaleTimeString().slice(-10, -6)} PM`;
 };
 
 const makeAPICall = (url) => {
@@ -21,7 +30,7 @@ const makeAPICall = (url) => {
     })
     .then(response => response.json())
     .then(data => updateWeatherUI(data))
-    .catch(err => console.log('error:', err));
+    .catch(err => = `error: ${err}`);
 };
 
 const inputChoice = 'Lagos';
