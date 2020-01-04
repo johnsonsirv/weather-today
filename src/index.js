fa551b305d60708cad20d8c81d92c6495ae707f6
Victor Johnson <johnsonsirv@gmail.com>
2020-01-04 07:27:51 +0100

    add toggle for temp scales

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './css/style.css';
 import importAll from './data';
 import preLoaderGIF from './images/loader.gif';
 
+const temperatureScale = { C: 0.0, F: 0.0 };
 const weatherAPIUrl = (location) => {
   const publicAPIKey = '3aab69399bf03eca438758bf6e33d18e';
   const weatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${publicAPIKey}`;
@@ -37,6 +38,11 @@ const togglePreLoader = (active = false) => {
 
 const weatherTempInFahrenheit = celcius => ((celcius * (9 / 5)) + 32).toFixed(2);
 
+const updateTempScales = (celcius) => {
+  temperatureScale.C = `${celcius} &#8451;`;
+  temperatureScale.F = `${weatherTempInFahrenheit(celcius)} &#8457;`;
+};
+
 const updateWeatherUI = (data) => {
   document.getElementById('main').style.backgroundImage = `url(${weatherBackgroundCover(data.weather[0].main)})`;
   document.getElementById('main').style.backgroundPosition = 'center';
@@ -44,7 +50,8 @@ const updateWeatherUI = (data) => {
   document.getElementById('weather-icon-2').src = weatherIcon(data.weather[0].icon);
   document.getElementById('weather-location').innerHTML = `${data.name}`.toUpperCase();
   document.getElementById('weather-temp').innerHTML = `${data.main.temp} &#8451;`;
-  document.getElementById('weather-temp-fah').innerHTML = `${weatherTempInFahrenheit(data.main.temp)} &#8457;`;
+  document.getElementById('toggle-temp').innerHTML = 'F';
+  updateTempScales(data.main.temp);
   document.getElementById('weather-date-time').innerHTML = calculatedDateTime(data.dt, data.timezone);
   document.getElementById('weather-main').textContent = `${data.weather[0].main}`.toUpperCase();
   document.getElementById('weather-description').textContent = `${data.weather[0].description}`.toUpperCase();
@@ -79,11 +86,22 @@ const loadDefaultWeather = () => {
   makeAPICall(url);
 };
 
+const toggleTempScale = (handle) => {
+  const trigger = handle;
+  document.getElementById('weather-temp').innerHTML = temperatureScale[trigger.textContent];
+  trigger.textContent = (trigger.textContent === 'F') ? 'C' : 'F';
+};
+
 document.getElementById('btn-search')
   .addEventListener('click', () => {
     const inputChoice = document.getElementById('search-city-input').value;
     const url = weatherAPIUrl(inputChoice);
     makeAPICall(url);
+  });
+
+document.getElementById('toggle-temp')
+  .addEventListener('click', () => {
+    toggleTempScale(document.getElementById('toggle-temp'));
   });
 
 loadDefaultWeather();
